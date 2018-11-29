73d60a42eda498a28812442a40875527b203d90c
Jaikangam Malangmei <jaikangam12@gmail.com>
2018-11-29 16:46:53 +0530

    create Product and use auth.api middleware to restrict

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -10,6 +10,7 @@ class Product extends Model
 	protected $fillable = [
 		'name','detail','stock','price','discount'
 	];
+	
     public function reviews()
     {
     	return $this->hasMany(Review::class); 
